a6f6aad552dac3834d22f61c50ea03763658d357
lrosen33 <lev_r@msn.com>
2024-03-04 10:56:27 -0500

    Add project files.

---
+++ DeploymentAppWithDb/Pages/Expenses/Create.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DeploymentAppWithDb.Data;
+using DeploymentAppWithDb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeploymentAppWithDb.Pages.Expenses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DeploymentAppWithDb.Data.ProjectContext _context;
+
+        public CreateModel(DeploymentAppWithDb.Data.ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["ExpenseTypeID"] = new SelectList(_context.ExpenseTypes, "ID", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public Expense Expense { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var expenseType = await _context.ExpenseTypes.FirstOrDefaultAsync(e => e.ID == Expense.ExpenseTypeID);
+            if (expenseType == null)
+            {
+                return Page();
+            }
+
+            ModelState.ClearValidationState(nameof(Expense));
+            Expense.ExpenseType = expenseType;
+            bool isValid = TryValidateModel(Expense, nameof(Expense));
+
+            if (isValid)
+            {
+                return Page();
+            }
+
+            _context.Expenses.Add(Expense);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ DeploymentAppWithDb/Pages/Expenses/Edit.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DeploymentAppWithDb.Data;
+using DeploymentAppWithDb.Models;
+
+namespace DeploymentAppWithDb.Pages.Expenses
+{
+    public class EditModel : PageModel
+    {
+        private readonly DeploymentAppWithDb.Data.ProjectContext _context;
+
+        public EditModel(DeploymentAppWithDb.Data.ProjectContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Expense Expense { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Expenses == null)
+            {
+                return NotFound();
+            }
+
+            var expense =  await _context.Expenses.Include(e => e.ExpenseType).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            bool modelState=ModelState.IsValid;
+            Expense = expense;
+
+           ViewData["ExpenseTypeID"] = new SelectList(_context.ExpenseTypes, "ID", "Name");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            var expenseType = await _context.ExpenseTypes.FirstOrDefaultAsync(e => e.ID == Expense.ExpenseTypeID);
+            if (expenseType==null)
+            {
+                return Page();
+            }
+
+            ModelState.ClearValidationState(nameof(Expense));
+            Expense.ExpenseType = expenseType;
+            bool isValid = TryValidateModel(Expense, nameof(Expense));
+
+            if (!isValid)
+            {
+                return Page();
+            }
+
+            _context.Entry(Expense).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseExists(Expense.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ExpenseExists(int id)
+        {
+          return _context.Expenses.Any(e => e.ID == id);
+        }
+    }
+}
